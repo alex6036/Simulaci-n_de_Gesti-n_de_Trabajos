74e8c01f6182bd567ad8f389ec5725431ce7e921
alex6036 <145665271+alex6036@users.noreply.github.com>
2025-05-14 07:44:39 +0000

    Comportamiento de los Algoritmos de Planificación

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,30 +1,33 @@
 # tests/test_scheduler.py
+
 import pytest
 from src.proceso import Proceso
 from src.scheduler import FCFSScheduler, RoundRobinScheduler
 
 def test_fcfs_scheduler():
     procesos = [
-        Proceso("P1", 5, 1),
-        Proceso("P2", 3, 2),
-        Proceso("P3", 2, 3)
+        Proceso(pid="P1", duracion=10, prioridad=2),
+        Proceso(pid="P2", duracion=5, prioridad=1),
+        Proceso(pid="P3", duracion=8, prioridad=3),
     ]
     scheduler = FCFSScheduler()
     gantt = scheduler.planificar(procesos)
-    assert gantt == [("P1", 0, 5), ("P2", 5, 8), ("P3", 8, 10)]
-    assert procesos[0].tiempo_inicio == 0
-    assert procesos[1].tiempo_inicio == 5
-    assert procesos[2].tiempo_fin == 10
+    
+    assert gantt[0] == ("P1", 0, 10)
+    assert gantt[1] == ("P2", 10, 15)
+    assert gantt[2] == ("P3", 15, 23)
 
 def test_round_robin_scheduler():
     procesos = [
-        Proceso("P1", 4, 1),
-        Proceso("P2", 3, 1)
+        Proceso(pid="P1", duracion=10, prioridad=2),
+        Proceso(pid="P2", duracion=5, prioridad=1),
+        Proceso(pid="P3", duracion=8, prioridad=3),
     ]
-    scheduler = RoundRobinScheduler(quantum=2)
+    scheduler = RoundRobinScheduler(quantum=5)
     gantt = scheduler.planificar(procesos)
-
-    assert gantt == [
-        ("P1", 0, 2), ("P2", 2, 4),
-        ("P1", 4, 6), ("P2", 6, 7)
-    ]
+    
+    assert gantt[0] == ("P1", 0, 5)
+    assert gantt[1] == ("P2", 5, 10)
+    assert gantt[2] == ("P3", 10, 15)
+    assert gantt[3] == ("P1", 15, 20)
+    assert gantt[4] == ("P3", 20, 23)
